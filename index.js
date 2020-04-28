4b19826438433cd2ec3d57d1aae55ce2f7f9501e
Alexandr <kore984134@gmail.com>
2020-04-28 19:07:37 +0300

    asd

--- index.js
+++ index.js
@@ -1,129 +1,119 @@
+const molesArr = Array.from(document.querySelectorAll(".cell"));
+const difficultChange = document.getElementById("changeDifficult");
+const startBtn = document.getElementById("btnStart");
+
 class WhackAMole {
 
-    // Properties used to initialize our
-    // Whack-a-Mole Game
-    constructor(startButton, moles, scoreOut, gameTimeLength, peepTimeMin, peepTimeMax){
-        // Game HTML Elements
+    constructor(startButton, moles, difficult){
+
         this.btnStart = startButton;
         this.moles = moles;
-        this.scoreOut = scoreOut;
+        this.difficult = difficult;
+        this.difficultTime = null;
+        this.gameStarted = false;
+        this.finishPoint = this.moles.length / 2;
+        this.playerScore = 0;
+        this.computerScore = 0;
+    }
 
-        // Game Images
-        this.moleImgSrc = 'images/mole.jpg';
-        this.moleBonkedImg = new Image();
-        this.moleBonkedImg.src = 'images/whacked-mole.jpg';
+    lose() {
+        alert("Computer win! You are LOSSER! AZAZAZAZAZA");
+    }
 
-        // Game Parameters
-        this.gameTime = gameTimeLength;
-        this.minPeepTime = peepTimeMin;
-        this.maxPeepTime = peepTimeMax;
-        this.numOfMoles = this.moles.length;
-
-        // Game State Variables
-        this.prevMoleNumber = null;
-        this.timeUp = false;
-        this.score = 0;
-        this.gameTimer = null;
-        this.peepTimer = null;
+    win(){
+        if (this.difficultTime === 500){
+            alert("Oh my fucking GOD what are you!?")
+        } else {
+            alert("You are win! Congratulation!!!");
+        }
     }
 
     init(){
-        this.score = 0;
-        this.scoreOut.text(this.score);
-        this.timeUp = false;
-        this.prevMoleNumber = null;
-        this.btnStart.text('Stop Game');
-        this.peep();
-        this.gameTimer = setTimeout(() => {
-            console.log('Game Over...');
-            this.btnStart.text('Start Game');
-            this.timeUp = true;
-        }, this.gameTime);
+        if (this.gameStarted === false){
+            this.playerScore = 0;
+            this.computerScore = 0;
+            this.gameStarted = true;
+            this.btnStart.textContent = 'Stop Game' ;
+            this.peep();
+        }
+
+    }
+
+    changeTime(){
+            switch (this.difficult.selectedIndex) {
+                case 0: {
+                    this.difficultTime = 1500;
+                    break;
+                }
+                case 1: {
+                    this.difficultTime = 1000;
+                    break;
+                }
+                case 2: {
+                    this.difficultTime = 500;
+                }
+            }
     }
 
     stop(){
         console.log('Game Stopped...');
-        this.btnStart.text('Start Game');
-        this.timeUp = true;
-        this.moles.removeClass('up');
+        this.btnStart.textContent = 'Start Game';
+        this.gameStarted = false;
+        this.moles.forEach((elem) => elem.className = "cell");
         clearInterval(this.peepTimer);
-        clearInterval(this.gameTimer);
     }
 
     peep(){
-        const time = this._randomTime(this.minPeepTime, this.maxPeepTime);
         const mole = this._randomMole(this.moles);
-        mole.addClass('up');
+        mole.classList.add('active');
         this.peepTimer = setTimeout(() => {
-            mole.removeClass('up');
-            if(this.timeUp === false){
-                this.peep();
+            if (mole.classList.contains("active")){
+                mole.classList.remove("active");
+                mole.classList.add("missed");
+                this.computerScore++;
+            } else if (mole.classList.contains("bonked")){
+                this.playerScore++;
             }
-        }, time);
+            if(this.playerScore < this.finishPoint && this.computerScore < this.finishPoint){
+                this.peep();
+            } else if (this.computerScore === this.finishPoint){
+                this.stop();
+                this.lose();
+            } else if (this.playerScore === this.finishPoint){
+                this.stop();
+                this.win();
+            }
+        }, this.difficultTime);
     }
 
-    bonk(mole) {
-        mole.attr('src', this.moleBonkedImg.src)
-            .removeClass('up')
-            .addClass('bonked')
-            .one('transitionend', () => {
-                mole.attr('src', this.moleImgSrc);
-                mole.removeClass('bonked');
-            });
-        this.score++;
-        this.scoreOut.text(this.score);
-    }
-
-    // Utility functions
-
-    // generate a random time to determine how long
-    // the moles stay up
-    _randomTime(min, max){
-        return Math.round(Math.random() * (max - min) + min);
-    }
-
-    // randomly selects one of the moles to display
     _randomMole(moles) {
-        const idx = Math.floor(Math.random() * this.numOfMoles);
-        const mole = moles.eq(idx);
-        if(idx === this.prevMoleNumber ) {
-            console.log('...same mole...try again...');
+        const moleIndex = Math.floor(Math.random() * moles.length);
+        const mole = moles[moleIndex];
+        if(mole.classList.contains("bonked") || mole.classList.contains("missed")) {
+            console.log('not this mole...');
             return this._randomMole(moles);
         }
-        this.prevMoleNumber = idx;
         return mole;
     }
 
 }
 
-// Get a new instance of the Whack A Mole
-// class
-// Parameters:
-// 1. Start Button
-// 2. Mole Image
-// 3. Score out
-// 4. Game Time Length (ms)
-// 5. Peep Time Min (ms)
-// 6. Peep Time Max (ms)
-const wam = new WhackAMole($('#btn-start'), $('.mole-pic'), $('#score-out'), 12000, 1000, 2000);
+const game = new WhackAMole(startBtn,molesArr,difficultChange);
 
-// Game Event Handlers
-wam.btnStart.click(function(){
-
-    if(wam.btnStart.text() === 'Start Game'){
-        wam.init();
+game.btnStart.addEventListener("click", function (){
+    if(game.btnStart.textContent === 'Start Game'){
+        game.changeTime();
+        game.init();
     }else{
-        wam.stop();
+        game.stop();
     }
-
 });
 
-wam.moles.click(function(){
-
-    if($(this).hasClass('bonked')){
-        return;
-    }
-
-    wam.bonk( $(this) );
-
+game.moles.forEach(function (elem) {
+    elem.addEventListener("click", function(){
+        if (this.classList.contains("active")){
+            this.classList.remove("active");
+            this.classList.add("bonked");
+        }
+    });
 });
